c6c8755f7a4d6a189fa8da52b90370baf1c006fc
Lucas Viana <oviana@outlook.com>
2022-09-06 16:09:00 -0300

    Add enabled control checking to onEdit hook

--- src/core/on-edit.feature.ts
+++ src/core/on-edit.feature.ts
@@ -1,10 +1,13 @@
+import { ss } from '@lib/constants';
 import { getRangeBelow, rangesOverlap } from '@lib/fuctions';
 import { SheetsOnEditEvent } from '@lib/models';
-import { watchedRanges } from '@utils/constants';
+import { NamedRange, watchedRanges } from '@utils/constants';
 import { calculateBreakEven } from './break-even.feature';
 
 export const onEdit = (e: SheetsOnEditEvent) => {
-  if (watchedRanges.some(range => rangesOverlap(getRangeBelow(range), e.range))) {
+  const isHookEnabled: boolean = ss.getRangeByName(NamedRange.ControlBreakEven).getValue();
+
+  if (isHookEnabled && watchedRanges.some(range => rangesOverlap(getRangeBelow(range), e.range))) {
     calculateBreakEven(false);
   }
 };
