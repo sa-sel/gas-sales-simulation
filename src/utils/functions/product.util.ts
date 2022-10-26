f75c4fa99a1377be3d073b07636a3a1bf68f58c8
Lucas Viana <oviana@outlook.com>
2022-10-26 01:02:46 -0300

    fix(product): Fix batch delete (deleting all products wold break sheet)

--- src/utils/functions/product.util.ts
+++ src/utils/functions/product.util.ts
@@ -5,7 +5,17 @@ export const getSelectedProductsIds = (): string[] =>
   fetchData<string>(sheets.products, { map: row => row[0], filter: row => row[row.length - 1] });
 
 /** Run a function for each selected product. Its parameter must be the product's ID cell. */
-export const manageSelectedProducts = (fn: (idCell: Range) => any): void =>
-  getSelectedProductsIds().forEach(id => manageDataInSheets(id, syncedDataSheets, fn));
+export const manageSelectedProducts = (fn: (idCell: Range) => any): void => {
+  const products = getSelectedProductsIds();
+  const lastProductId = products.pop();
+
+  products.forEach(id => manageDataInSheets(id, syncedDataSheets, fn));
+
+  // this is in order to prevent a bug that *may* happen
+  // if you delete all existing products at once
+  if (lastProductId) {
+    manageDataInSheets(lastProductId, syncedDataSheets, fn);
+  }
+};
 
 export const unselectProducts = () => getRangeBelow(NamedRange.SelectedProductsHeader).uncheck();
